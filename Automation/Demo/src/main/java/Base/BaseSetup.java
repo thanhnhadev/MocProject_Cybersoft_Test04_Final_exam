35fde587e94da46e1bf8a15acfde934b53ed20f4
thanhnhadev <thanhnhadev@gmail.com>
2025-07-07 14:49:08 +0700

    pom-report

--- Automation/Demo/src/main/java/Base/BaseSetup.java
+++ Automation/Demo/src/main/java/Base/BaseSetup.java
@@ -1,5 +1,7 @@
 package Base;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,6 +13,7 @@ import java.time.Duration;
 public class BaseSetup {
     private WebDriver driver;
     static String driverPath = "resources\\drivers\\";
+    static Logger logger = LogManager.getLogger(BaseSetup.class);
     public WebDriver getDriver(){
         return driver;
     }
---
+++ Automation/Demo/src/main/java/utils/ExtentManager.java
@@ -0,0 +1,20 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+    private static ExtentReports extent;
+    public static ExtentReports createInstance() {
+        String reportPath = "./reports/ExtentReport.html";
+        ExtentSparkReporter reporter = new ExtentSparkReporter(reportPath);
+        reporter.config().setDocumentTitle("Automation Test Report");
+        reporter.config().setReportName("Test Results");
+        reporter.config().setTheme(com.aventstack.extentreports.reporter.configuration.Theme.STANDARD);
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester", "susan ta");
+        extent.setSystemInfo("Environment", "Production");
+        return extent;
+    }
+}
