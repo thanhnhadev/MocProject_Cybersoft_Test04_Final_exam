8c254864ed895bf6f71c740f75e910c0b22b596a
thanhnhadev <thanhnhadev@gmail.com>
2025-07-18 09:15:18 +0700

    registerv3.1

--- Automation/Demo/src/main/java/Constants/ConfigData.java
+++ Automation/Demo/src/main/java/Constants/ConfigData.java
@@ -4,5 +4,6 @@ public class ConfigData {
     public static final String base_url="https://demo5.cybersoft.edu.vn/";
     public static String signinUrl = "https://demo5.cybersoft.edu.vn/login";
     public static String registerUrl = "https://demo5.cybersoft.edu.vn/register";
+    public static String profileUrl="https://demo5.cybersoft.edu.vn/profile";
     public static  String regex = "^.{6,32}$";
 }
