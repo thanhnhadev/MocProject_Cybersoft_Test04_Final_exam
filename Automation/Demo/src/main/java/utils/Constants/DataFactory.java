0e2439ae781cc39bb42c9d2e944d6657c89745a5
Thanhnhadev <thanhnhadev@gmail.com>
2025-08-21 09:10:10 +0700

    DataFactory

--- Automation/Demo/src/main/java/utils/Constants/DataFactory.java
+++ Automation/Demo/src/main/java/utils/Constants/DataFactory.java
@@ -10,21 +10,21 @@ public class DataFactory {
     @DataProvider(name = "data_Login_Excel", parallel = true)
     public Object[][] dataLogin() {
         ExcelHelper excelHelper = new ExcelHelper();
-        Object[][] data = excelHelper.getDataHashTable( ConfigData.Excel, "Login", 1, 5);
+        Object[][] data = excelHelper.getDataHashTable( ConfigData.Excel, "Login", 1, 1);
         LogUtils.info( "Login Data from Excel: " + data);
         return data;
     }
     @DataProvider(name = "data_LoginFail_Excel", parallel = true)
     public Object[][] dataLogin_Fail() {
         ExcelHelper excelHelper = new ExcelHelper();
-        Object[][] data = excelHelper.getDataHashTable( ConfigData.Excel, "Login_Fail", 1, 5);
+        Object[][] data = excelHelper.getDataHashTable( ConfigData.Excel, "Login_Fail", 1, 1);
         LogUtils.info("Login Data from Excel: " + data);
         return data;
     }
     @DataProvider(name = "data_Register_Excel")
     public Object[][] dataJoin() {
         ExcelHelper excelHelper = new ExcelHelper();
-        Object[][] data = excelHelper.getDataHashTable( ConfigData.Excel, "Login", 1,5);
+        Object[][] data = excelHelper.getDataHashTable( ConfigData.Excel, "Login", 1,1);
         LogUtils.info("Login Data from Excel: " + data);
         return data;
     }
